c130a2146b234c079faca03ec8b37e863e36fe10
Syeda Sana Zehra <sana.zehra@tpsonline.com>
2020-04-08 22:46:39 +0500

    code: open file and read

--- msg_sender_app/lib/main.dart
+++ msg_sender_app/lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:spreadsheet_decoder/spreadsheet_decoder.dart';
 
 void main() => runApp(MyApp());
 
@@ -26,10 +30,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-  
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -38,12 +40,82 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'App init',
-            ),
+            RaisedButton(
+              onPressed: _pickFile,
+              child: Text('Open File'),
+              color: Colors.purple,
+            )
           ],
         ),
       ),
     );
   }
+
+  void _pickFile() async {
+    File fileXlxs = await FilePicker.getFile();
+    print(fileXlxs.path);
+    var xcelRows = parseFile(fileXlxs.path);
+    //fillData(xcelRows);
+  }
+
+  List<List<dynamic>> parseFile(String path) {
+    try {
+      var bytes = File(path).readAsBytesSync();
+      var decoder = SpreadsheetDecoder.decodeBytes(bytes, update: true);
+      for (var table in decoder.tables.keys) {
+        for (var row in decoder.tables[table].rows) {
+          print("$row");
+          // print(row.toString());
+        }
+        return decoder.tables[table].rows;
+      }
+      return null;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  List<Data> fillData(List<List<dynamic>> xcelRows) {
+    List<Data> dataRows = new List<Data>();
+
+    for (var row in xcelRows) {
+      print(row[0]?.toString());
+      print('======');
+      dataRows.add(Data(
+          serialNo: row[0]?.toString(),
+          flatNo: row[1],
+          contactName: row[2],
+          noOfMonths: row[3]?.toString(),
+          amount: row[4],
+          contactNumber: row[5],
+          message: row[6]));
+    }
+
+    dataRows.removeAt(0);
+
+    for(var item in dataRows)
+    {
+      print(item.message);
+    }
+
+  }
+}
+
+class Data {
+  final String serialNo;
+  final String flatNo;
+  final String contactName;
+  final String noOfMonths;
+  final String amount;
+  final String contactNumber;
+  final String message;
+
+  Data(
+      {this.serialNo,
+      this.flatNo,
+      this.contactName,
+      this.noOfMonths,
+      this.amount,
+      this.contactNumber,
+      this.message});
 }
